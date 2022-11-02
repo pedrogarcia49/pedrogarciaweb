bca7e4a1c765dfa8ea29fd74497018b336a050dd
Pedro <pedrogarciaok9@gmail.com>
2022-11-01 22:23:37 -0300

    server config change and icon added

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const colors = require("colors")
 const path = require('path')
 const app = express()
 
-app.listen(3000)
+app.listen(3100)
 
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
